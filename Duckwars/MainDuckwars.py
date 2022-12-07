f5d259fdd0a94fc53a09df82330200201d78f1ec
Viktor Gonodanov-Meydbray <vgmeydbray@gmail.com>
2022-12-06 18:23:16 -0800

    Added play,options,and quit button
    quit button works

--- Duckwars/MainDuckwars.py
+++ Duckwars/MainDuckwars.py
@@ -1,4 +1,5 @@
 #imports
+import sys
 from direct.showbase.ShowBase import *
 from direct.gui.OnscreenText import *
 from direct.gui.DirectGui import *
@@ -11,18 +12,28 @@ class Game(ShowBase):
         global font
         #inits showbase
         ShowBase.__init__(self)
+        #disables default camera controls
+        self.disableMouse()
         #gets window properties
         wp=WindowProperties()
         #sets size
         wp.setSize(base.pipe.getDisplayWidth(),base.pipe.getDisplayHeight())
-        self.win.requestProperties(wp)
+        base.win.requestProperties(wp)
         #makes font
         font=loader.loadFont('font.ttf')
+        font.setPixelsPerUnit(100)
+        font.setPageSize(512,512)
         #starts game by opening menu screen
         Game.Menu()
     def Menu():
         menuScreen=DirectDialog(frameSize=(0,0,0,0))
-        nameText=OnscreenText(text="Duckwars:Beaks Of Rage",scale=0.3,pos=(0,0.7),fg=(255,255,255,255),font=font)
+        nameText=OnscreenText(text="Duckwars:Beaks Of Rage",parent=menuScreen,scale=0.3,pos=(0,0.7),fg=(255,255,255,255),shadow=(0,0,0,255),font=font)
+        playButton=DirectButton(text="Play",parent=menuScreen,scale=0.1,pos=(-1,0,0.3),frameSize=(-3,3,-0.4,0.9),command=menuScreen.hide)
+        optionsButton=DirectButton(text="Options",parent=menuScreen,scale=0.1,pos=(-1,0,0.1),frameSize=(-3,3,-0.4,0.9),command=menuScreen.hide)
+        quitButton=DirectButton(text="Quit",parent=menuScreen,scale=0.1,pos=(-1,0,-0.1),frameSize=(-3,3,-0.4,0.9),command=Game.Quit)
+    def Quit():
+        base.graphicsEngine.removeAllWindows()
+        sys.exit()
 #runs game
 game=Game()
 game.run()
