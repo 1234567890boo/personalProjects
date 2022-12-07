1f63b62b118d45b41b1bb5f06612651629f9f26b
Viktor Gonodanov-Meydbray <vgmeydbray@gmail.com>
2022-12-06 22:43:29 -0800

    all buttons work now

--- Duckwars/MainDuckwars.py
+++ Duckwars/MainDuckwars.py
@@ -23,17 +23,36 @@ class Game(ShowBase):
         font=loader.loadFont('font.ttf')
         font.setPixelsPerUnit(100)
         font.setPageSize(512,512)
-        #starts game by opening menu screen
+        #inits options screen
+        Game.Options()
+        optionsScreen.hide()
+        #inits and opens menu
         Game.Menu()
+    def Load(whatLoading):
+        if whatLoading=="Play":
+            menuScreen.hide()
+            optionsScreen.hide()
+            Game.Play()
     def Menu():
+        global menuScreen
         menuScreen=DirectDialog(frameSize=(0,0,0,0))
         nameText=OnscreenText(text="Duckwars:Beaks Of Rage",parent=menuScreen,scale=0.3,pos=(0,0.7),fg=(255,255,255,255),shadow=(0,0,0,255),font=font)
-        playButton=DirectButton(text="Play",parent=menuScreen,scale=0.1,pos=(-1,0,0.3),frameSize=(-3,3,-0.4,0.9),command=menuScreen.hide)
-        optionsButton=DirectButton(text="Options",parent=menuScreen,scale=0.1,pos=(-1,0,0.1),frameSize=(-3,3,-0.4,0.9),command=menuScreen.hide)
+        playButton=DirectButton(text="Play",parent=menuScreen,scale=0.1,pos=(-1,0,0.3),frameSize=(-3,3,-0.4,0.9),command=Game.Load,extraArgs=["Play"])
+        optionsButton=DirectButton(text="Options",parent=menuScreen,scale=0.1,pos=(-1,0,0.1),frameSize=(-3,3,-0.4,0.9),command=Game.Options)
         quitButton=DirectButton(text="Quit",parent=menuScreen,scale=0.1,pos=(-1,0,-0.1),frameSize=(-3,3,-0.4,0.9),command=Game.Quit)
+    def Options():
+        global optionsScreen
+        optionsScreen=DirectDialog(frameSize=(-1.2,1.2,-0.7,0.7),pos=(0,0,-0.1))
+        doneButton=DirectButton(text="Done",parent=optionsScreen,scale=0.1,pos=(-0.2,0,-0.6),command=optionsScreen.hide)
+        quitButton=DirectButton(text="Quit",parent=optionsScreen,scale=0.1,pos=(0.2,0,-0.6),command=Game.Quit)
+    def Play():
+        environment=loader.loadModel("environment").reparentTo(render)
     def Quit():
         base.graphicsEngine.removeAllWindows()
         sys.exit()
 #runs game
 game=Game()
 game.run()
+
+
+
