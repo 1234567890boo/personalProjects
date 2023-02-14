baa56387c54980f7c4a4e3fab24a46f9ab6a8eb4
Viktor Gonodanov-Meydbray <vgmeydbray@gmail.com>
2023-02-14 10:53:14 -0800

    refactored code to use self instead of global

--- Duckwars/MainDuckwars.py
+++ Duckwars/MainDuckwars.py
@@ -3,109 +3,96 @@ from direct.showbase.ShowBase import ShowBase
 from panda3d.bullet import *
 from panda3d.core import *
 from direct.task import Task
-
-#Game Class
+#class with all game logic
 class Game(ShowBase):
-  #initializes game things
+  #initializes game stuff
   def __init__(self):
-    #sets globals variables
-    global bulletWorld,wp,keyMap
-    #initializes the game window
+    #inits window
     ShowBase.__init__(self)
-    #inits physics simulation and gravity
-    bulletWorld=BulletWorld()
-    bulletWorld.setGravity(Vec3(0,0,-9.81))
-    #gets window properties
-    wp=WindowProperties()
-    #sets size
-    width=base.pipe.getDisplayWidth()
-    height=base.pipe.getDisplayHeight()
-    wp.setSize(width,height)
+    #inits game physics
+    self.bulletWorld = BulletWorld()
+    self.bulletWorld.setGravity(Vec3(0,0,-9.81))
+    #inits window properties to change windwo size and cursor settings
+    self.wp = WindowProperties()
+    #changes screen size
+    self.width = base.pipe.getDisplayWidth()
+    self.height = base.pipe.getDisplayHeight()
+    self.wp.setSize(self.width,self.height)
     #for testing
-    wp.setSize(500,500)
-    #for testing
-    base.win.requestProperties(wp)
-    #calls play function
-    Game.Play()
-    #disables mouse controlls
+    self.wp.setSize(500,500)
+    #acctually sets screen size
+    base.win.requestProperties(self.wp)
+    #disables default mouse controls
     base.disableMouse()
-  #hides mouse
-  def mouseHide():
-    wp.setCursorHidden(True)
-    base.win.requestProperties(wp)
-  #shows mouse
-  def mouseShow():
-    wp.setCursorHidden(False)
-    base.win.requestProperties(wp)
-  #updates the physics simulation every frame
-  def updatePhys(task):
-    bulletWorld.doPhysics(globalClock.getDt())
+    #starts game
+    self.Play()
+  #task for updating physics
+  def updatePhys(self, task):
+    self.bulletWorld.doPhysics(globalClock.getDt())
     return task.cont
-  #player movement task
-  def playerMovement(task):
-    #keyboard controls
-    isJump=bulletWorld.contactTest(playerNode.node()).getNumContacts()
+  #task for player movement
+  def playerMovement(self, task):
+    #checks if the player is on the ground
+    isJump = self.bulletWorld.contactTest(self.playerNode.node()).getNumContacts()
     if isJump!=0:isJump=True
-    #mouse movement
-    base.camera.setP(base.camera,base.win.getPointer(0).getY()-int(base.win.getYSize()/2))
-    playerNode.setH(playerNode,base.win.getPointer(0).getX()-int(base.win.getXSize()/2))
-    #resets the cursor to the center of the screen
-    base.win.movePointer(0,base.win.getXSize()//2,base.win.getYSize()//2)
-    #caps camera movement
-    if base.camera.getP()>80:base.camera.setP(79)
-    if base.camera.getP()<-60:base.camera.setP(-61)
-    if base.camera.getH()!=0:base.camera.setH(0)
-    #resets player and camera yaw
-    playerNode.setR(0)
+    #for camera mouse looking
+    base.camera.setP(base.camera, base.win.getPointer(0).getY() - int(base.win.getYSize()/2))
+    self.playerNode.setH(self.playerNode, base.win.getPointer(0).getX() - int(base.win.getXSize()/2))
+    #moves cursor to center of screen
+    base.win.movePointer(0, base.win.getXSize()//2, base.win.getYSize()//2)
+    #camera movement capping
+    if base.camera.getP() > 80:base.camera.setP(79)
+    if base.camera.getP() < -60:base.camera.setP(-61)
+    if base.camera.getH() != 0:base.camera.setH(0)
+    #makes sure camera or player dont roll
+    self.playerNode.setR(0)
     base.camera.setR(0)
-    #makes task run again
+    #redos task
     return task.cont
-  #procedurally makes collision mesh for models
-  def modelHBMakeRender(inputModel,mass,friction,pos):
-    #inputs model
-    inputModel=loader.loadModel(inputModel)
-    #inits tirangle meshes
-    outputBulletMesh=BulletTriangleMesh()
-    #makes tiangle mesh be the shape of the geom and finds geom
+  #makes physics mesh for models
+  def modelHBMakeRender(self,inputModel,mass,friction,pos):
+    #loads model
+    inputModel = loader.loadModel(inputModel)
+    #makes mesh thats empty
+    outputBulletMesh = BulletTriangleMesh()
+    #adds triangles to mesh that is the shape of the model
     outputBulletMesh.addGeom(inputModel.findAllMatches("**/+GeomNode").getPath(0).node().getGeom(0))
-    #makes charecter controller or rigidbody
+    #adds rigid body node to render
     np=render.attachNewNode(BulletRigidBodyNode(str(inputModel)))
-    #sets its starting position
+    #sets position of node
     np.setPos(pos)
-    #sets mass for the mesh
+    #sets mass of node
     np.node().setMass(mass)
+    #sets friction of node
     np.node().setFriction(friction)
     #adds mesh to the node
     np.node().addShape(BulletTriangleMeshShape(outputBulletMesh,dynamic=True))
-    #renders and attaches mesh to node
-    bulletWorld.attachRigidBody(np.node())
-    #returns model and np
-    return inputModel,np
-  #play function where gameplay will go
-  def Play():
-    global playerNode
-    #makes collider debugand shows it
-    debugNode=BulletDebugNode("debug")
-    debugNode.showBoundingBoxes(True)
-    debugNP = render.attachNewNode(debugNode)
-    debugNP.show()
-    bulletWorld.setDebugNode(debugNP.node())
-    #adds mesh and renders boxing ring
-    boxingRing,boxingRingNode=Game.modelHBMakeRender("Models/BoxingRing.glb",0,1,(0,0,-2))
-    boxingRing.reparentTo(boxingRingNode)
-    #Adds mesh and renders player
-    player,playerNode=Game.modelHBMakeRender("Models/Player.glb",1,1,(0,0,0))
-    player.reparentTo(playerNode)
-    #reparents camera to player and sets position to the head
-    base.camera.reparentTo(playerNode)
-    base.camera.setPos(0,1,1)
-    #sets cursor to middle
-    base.win.movePointer(0,base.win.getXSize()//2,base.win.getYSize()//2)
-    #starts the physics
-    taskMgr.add(Game.updatePhys,"updatePhys",sort=1)
-    taskMgr.add(Game.playerMovement,"playerMovement",sort=2)
-    #hides Mouse
-    Game.mouseHide()
-#runs the game
-game=Game()
+    #makes node have physics
+    self.bulletWorld.attachRigidBody(np.node())
+    #returns thigs i need for the future
+    return inputModel, np
+  #play function
+  def Play(self):
+    #makes colider mesh and other physics things show up
+    self.debugNode=BulletDebugNode("debug")
+    self.debugNode.showBoundingBoxes(True)
+    self.debugNP=render.attachNewNode(self.debugNode)
+    self.debugNP.show()
+    self.bulletWorld.setDebugNode(self.debugNP.node())
+    #makes boxing ring model and mesh and
+    self.boxingRing,self.boxingRingNode=self.modelHBMakeRender("Models/BoxingRing.glb", 0, 1, (0,0,-2))
+    self.boxingRing.reparentTo(self.boxingRingNode)
+    #makes player mesh and model
+    self.player, self.playerNode=self.modelHBMakeRender("Models/Player.glb", 1, 1, (0,0,0))
+    self.player.reparentTo(self.playerNode)
+    #makes camera follow player
+    base.camera.reparentTo(self.playerNode)
+    base.camera.setPos(0, 1, 1)
+    #puts the cursor in the center of the screen
+    base.win.movePointer(0, base.win.getXSize()//2, base.win.getYSize()//2)
+    #starts the tasks
+    taskMgr.add(self.updatePhys, "updatePhys", sort=1)
+    taskMgr.add(self.playerMovement, "playerMovement", sort=2)
+#runs the game class
+game = Game()
 game.run()
