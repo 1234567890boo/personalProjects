71be485a192d779baa8df1681397da97f55d3fa5
Viktor Gonodanov-Meydbray <vgmeydbray@gmail.com>
2024-04-22 09:07:07 -0700

    Changed sprite to use dirty sprite
    switched to dirty sprites for built in layering. makes code simpler.

--- ParilaxBGTest/ParilaxBGTest.py
+++ ParilaxBGTest/ParilaxBGTest.py
@@ -3,15 +3,18 @@ import pygame,os
 #inits pygame
 pygame.init()
 #variables
-x,y=1014,576
+x,y=1004,576
 direction="none"
 files=next(os.walk("Layers"))#gets all files in layers folder
 files=int(len(files[2]))#gets number of files
+#makes background
+bg=pygame.Surface((x,y))
+bg.fill((50,50,50))
 #sets screen size and name
-screen=pygame.display.set_mode((x,y),flags=pygame.SCALED,vsync=True)
+screen=pygame.display.set_mode((x,y),pygame.SCALED,vsync=True)
 pygame.display.set_caption("ParilaxBGTest")
 #class for sprite for the paralax effect
-class BackgroundBlock(pygame.sprite.Sprite):
+class BackgroundBlock(pygame.sprite.DirtySprite):
     #init function
     def __init__(self,img,xpos,layer):
         super().__init__()
@@ -20,35 +23,33 @@ class BackgroundBlock(pygame.sprite.Sprite):
         self.rect=self.image.get_rect()
         self.rect.x=xpos
     #draw and movement function
-    def draw(self):
-        screen.blit(self.image,self.rect)
-        if direction=="right":sprite.rect.x+=self.layer
-        elif direction=="left":sprite.rect.x-=self.layer
+    def update(self):
+        if direction=="right":self.rect.move_ip(self.layer,0)
+        elif direction=="left":self.rect.move_ip(-self.layer,0)
         if self.rect.x>x or self.rect.x<-x:self.rect.x=-self.rect.x
-#makes layers, layer 1 "closest" layer
-layerList=[]
-for l in range(0,files,1):
-    layerName="layer"+str(l+1)
-    layerName=pygame.sprite.Group()
-    layerList.append(layerName)
-#adds sprites to layers based on name
-for e in range(len(layerList),0,-1):
-    for i in range(0,2,1):
-        layerSprite=BackgroundBlock("Layers/layer"+str(-e+len(layerList)+1)+".png",x*i,e)
-        layerSprite.add(layerList[e-1])
+        self.dirty=1
+#make sprites and adds to sprite list
+sprites=[]
+for i in range(0,files,1):
+    for n in range (0,2,1):
+        sprite=BackgroundBlock("Layers/layer"+str(i+1)+".png",n*x,i+1)
+        sprites.append(sprite)
+allSprites=pygame.sprite.LayeredDirty((sprites))
 #main Runloop
 while True:
     #clears screen
-    screen.fill((50,50,50))
+    allSprites.clear(screen,bg)
     #gets keypresses and button click for closing gameand moving the backgound
     for event in pygame.event.get():
         if event.type==pygame.QUIT:os._exit(0)
         elif event.type==pygame.KEYDOWN:
             if event.key==pygame.K_LEFT:direction="left"
             elif event.key==pygame.K_RIGHT:direction="right"
+            elif event.key==pygame.K_ESCAPE:os._exit(0)
         elif event.type==pygame.KEYUP:direction="none"
-    #renders the layers
-    for layer in layerList:
-        for sprite in layer:sprite.draw()
+        
+    rects=allSprites.draw(screen)
+    for sprite in allSprites:sprite.update()
     #updates the screen
-    pygame.display.update()
+    pygame.display.update(rects)
+
