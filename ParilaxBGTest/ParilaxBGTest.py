74975569f71dbfd5f20570cbbafa00184b2fcd51
Viktor Gonodanov-Meydbray <vgmeydbray@gmail.com>
2024-04-22 23:20:48 -0700

    added more movement
    made it able to move up and down and a commented out test for zooming in when moved up or down

--- ParilaxBGTest/ParilaxBGTest.py
+++ ParilaxBGTest/ParilaxBGTest.py
@@ -4,7 +4,7 @@ import pygame,os
 pygame.init()
 #variables
 x,y=1004,576
-direction="none"
+directionx,directiony="none","none"
 files=next(os.walk("Layers"))#gets all files in layers folder
 files=int(len(files[2]))#gets number of files
 #makes background
@@ -20,15 +20,27 @@ class BackgroundBlock(pygame.sprite.DirtySprite):
         super().__init__()
         self.layer=layer
         self.image=pygame.image.load(img).convert_alpha()
+        self.origImage=self.image
         self.rect=self.image.get_rect()
         self.rect.x=xpos
     #draw and movement function
     def update(self):
-        if direction=="right":self.rect.move_ip(self.layer,0)
-        elif direction=="left":self.rect.move_ip(-self.layer,0)
+        #for moving lets and right
+        if directionx=="right":self.rect.move_ip(self.layer,0)
+        elif directionx=="left":self.rect.move_ip(-self.layer,0)
+        #for moving up and down
+        if directiony=="zoom in" and self.layer!=1:
+            self.rect.move_ip(0,self.layer)
+            '''self.scaled=pygame.transform.scale_by(self.origImage,(1.01))
+            self.origImage=self.image
+            self.image.fill((0,0,0,0))
+            self.image.blit(self.scaled,(0,0))'''
+        elif directiony=="zoom out" and self.rect.y>0:self.rect.move_ip(0,-self.layer)
+        #for making it "inifinite"
         if self.rect.x>x or self.rect.x<-x:self.rect.x=-self.rect.x
+        #sets flag for redrawing
         self.dirty=1
-#make sprites and adds to sprite list
+#make sprites and adds to sprite list. first layer is furthest layer
 sprites=[]
 for i in range(0,files,1):
     for n in range (0,2,1):
@@ -43,10 +55,12 @@ while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:os._exit(0)
         elif event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_LEFT:direction="left"
-            elif event.key==pygame.K_RIGHT:direction="right"
+            if event.key==pygame.K_LEFT:directionx="left"
+            elif event.key==pygame.K_RIGHT:directionx="right"
+            elif event.key==pygame.K_UP:directiony="zoom in"
+            elif event.key==pygame.K_DOWN:directiony="zoom out"
             elif event.key==pygame.K_ESCAPE:os._exit(0)
-        elif event.type==pygame.KEYUP:direction="none"
+        elif event.type==pygame.KEYUP:directionx,directiony="none","none"
         
     rects=allSprites.draw(screen)
     for sprite in allSprites:sprite.update()
